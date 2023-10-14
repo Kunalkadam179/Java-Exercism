a88626a710600539b30b9384c6f97bf4b86a6b9f
Kunalkadam179 <89585888+Kunalkadam179@users.noreply.github.com>
2023-10-14 17:49:19 +0530

    Add files via upload

---
+++ error-handling/src/main/java/CustomCheckedException.java
@@ -0,0 +1,13 @@
+class CustomCheckedException extends Exception {
+
+    CustomCheckedException() {
+        super();
+//        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    }
+
+    CustomCheckedException(String message) {
+        super(message);
+//        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    }
+
+}
